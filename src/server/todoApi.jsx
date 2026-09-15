a807852fc715966f507d8e214554f4750988343f
anhoangv <aan.vuhoang@gmail.com>
2026-09-16 02:19:47 +0700

    refactor: configure api url with environment variable

--- src/server/todoApi.jsx
+++ src/server/todoApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL = "http://localhost:5000/api/todos";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getTodos = () => {
     return axios.get(API_URL);
